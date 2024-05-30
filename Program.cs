51485533e26449dfe5331651fd5b48f1f9dba4ee
Alexandr Yunusov <lionalex02@ya.ru>
2024-05-30 16:41:39 +0500

    Laba 9.01

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using HoplessProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.RegularExpressions;
 
 
 namespace HoplessProject
@@ -11,75 +12,133 @@ namespace HoplessProject
     public class Number : Token
     {
         public double number;
+        public bool isVariable;
     }
 
     public class Operation : Token
     {
         public char operation;
     }
-
+    public class Function : Token
+    {
+        public char function;
+    }
+    public class Other : Token
+    {
+        public char other;
+    }
     public class Parenthesis : Token
     {
         public bool bracket;
     }
-    public class Tester
+
+    public class Program
     {
         public static void Main()
         {
+            Console.Write("Введите значение переменной x: ");
+            double x = 10.0;
+            // double x = Convert.ToDouble(Console.ReadLine());
             Console.Write("Введите выражение: ");
-            string input = Console.ReadLine()!.Replace("\t", " ").Replace(" ", "");
+            string input = Console.ReadLine()!.ToLower()
+                .Replace("\t", " ")
+                .Replace(" ", "")
+                .Replace(".", ",")
+                .Replace("log", "l")
+                .Replace("sqrt", "q")
+                .Replace("rt", "r")
+                .Replace("sin", "s")
+                .Replace("cos", "c")
+                .Replace("tg", "t")
+                .Replace("ctg", "g")
+                .Replace("asin", "a")
+                .Replace("abs", "b")
+                .Replace("ln", "n")
+                .Replace("lg", "i");
+
             List<Token> postPars = Parsing(input);
+            Print(postPars);
             List<Token> rpn = PolishChange(postPars);
-            double absoluteFinale = Calculating(rpn);
+            Print(rpn);
+            double absoluteFinale = Calculating(rpn, x);
+
             Console.Write("Выражение в виде ОПЗ: ");
-            Print(rpn, new(), new(), new());
+            Print(rpn);
             Console.WriteLine("Итог выражения: " + string.Join(" ", absoluteFinale));
         }
+
         public static List<Token> Parsing(string input)
         {
             List<Token> finals = new();
             string number = "";
             foreach (char tokens in input)
             {
-                if (tokens != ' ')
+                if (tokens.Equals('('))
                 {
-                   if (tokens.Equals('('))
-                    {
-                        Parenthesis par = new();
-                        par.bracket = true;
-                        finals.Add(par);
-                    }
-                    else if (tokens.Equals(')'))
-                    {
-                        Parenthesis par = new();
-                        par.bracket = false;
-                        finals.Add(par);
-                    }
-                    else if (tokens.Equals('+') ^ tokens.Equals('-') ^ tokens.Equals('*') ^ tokens.Equals('/'))
-                    {
-                        if (number != "")
-                        {
-                            Number digits = new();
-                            digits.number = Convert.ToDouble(number);
-                            finals.Add(digits);
-                        } 
-                        Operation op = new();
-                        op.operation = tokens;
-                        finals.Add(op);
-                        number = "";
-                    }
-                    else 
-                        number += tokens;
+                    Clearing(number, finals);
+                    Parenthesis par = new();
+                    par.bracket = true;
+                    finals.Add(par);
                 }
+                else if (tokens.Equals(')'))
+                {
+                    Clearing(number, finals);
+                    Parenthesis par = new();
+                    par.bracket = false;
+                    finals.Add(par);
+                    number = "";
+                }
+                else if (tokens.Equals(';'))
+                {
+                    Clearing(number, finals);
+                    Other obj = new();
+                    finals.Add(obj);
+                    number = "";
+                }
+                else if (tokens.Equals('x'))
+                {
+                    Clearing(number, finals);
+                    Number digits = new();
+                    digits.isVariable = true;
+                    finals.Add(digits);
+                    number = "";
+                }
+                else if (tokens.Equals('+') ^ tokens.Equals('-') ^ tokens.Equals('*') ^ tokens.Equals('/') ^ tokens.Equals('^') ^ tokens.Equals('l') ^ tokens.Equals('r') )
+                {
+                    Clearing(number, finals);
+                    Operation op = new();
+                    op.operation = tokens;
+                    finals.Add(op);
+                    number = "";
+                }
+                else if ( tokens.Equals('s') ^ tokens.Equals('q') ^ tokens.Equals('t') ^ tokens.Equals('g') ^ tokens.Equals('c') ^ tokens.Equals('a') ^ tokens.Equals('b') ^ tokens.Equals('n') ^ tokens.Equals('i'))
+                {
+                    Clearing(number, finals);
+                    Function digits = new();
+                    digits.function = tokens;
+                    finals.Add(digits);
+                }
+                else if (char.IsDigit(tokens) ^ tokens.Equals(','))
+                {
+                    number += tokens;
+                }
+                else
+                    Console.WriteLine("Неизвестный символ");
             }
+            Clearing(number, finals);
+            return finals;
+        }
+
+        public static void Clearing(string number, List<Token> finals)
+        {
             if (number != "")
             {
                 Number digits = new();
-                digits.number = Convert.ToDouble(number);
+                digits.number = double.Parse(number);
                 finals.Add(digits);
             }
-            return finals;
         }
+
         public static double PriorityOper(Token priority)
         {
             if (priority is Operation)
@@ -87,21 +146,28 @@ namespace HoplessProject
                 return ((Operation)priority).operation switch
                 {
                     '+' or '-' => 1,
-                    '*' or '/' => 2,
+                    '*' or '/' or '^' => 2,
+                    _ => 0
+                };
+            }
+            else if (priority is Function)
+            {
+                return ((Function)priority).function switch
+                {
+                    'q' or 'c' or 's' or 'g' or 't' or 'a' or 'b' => 3,
+                    'l' or 'r' or 'i' or 'n' => 4,
                     _ => 0
                 };
             }
             return 0;
         }
-
         public static List<Token> PolishChange(List<Token> postPars)
         {
             Stack<Token> boofer = new();
             List<Token> finals = new();
             foreach (Token tokens in postPars)
             {
-                Console.WriteLine(tokens);
-                if (tokens is Operation)
+                if (tokens is Operation or Function)
                 {
                     if (boofer.Count() > 0 && PriorityOper(boofer.Peek()) >= PriorityOper(tokens))
                     {
@@ -125,7 +191,11 @@ namespace HoplessProject
                     }
                 }
                 else if (tokens is Number)
+                {
                     finals.Add(tokens);
+                }
+                else
+                    continue;
             }
             while (boofer.Count() > 0)
             {
@@ -134,64 +204,115 @@ namespace HoplessProject
             return finals;
         }
 
-
-        public static double GetNumber(Number number1, Number number2, Operation priority)
+        public static double GetNumber(Number number1, Number number2, Token priority)
         {
+            if (priority is Operation)
+            {
                 return ((Operation)priority).operation switch
                 {
                     '+' => number1.number + number2.number,
                     '-' => number1.number - number2.number,
                     '*' => number1.number * number2.number,
                     '/' => number1.number / number2.number,
+                    'l' => Math.Log(number2.number, number1.number),
+                    '^' => Math.Pow(number1.number,number2.number),
+                    'r' => Math.Pow(number2.number, 1.0 / number1.number),
                     _ => 0.0,
                 };
+            }
+            else
+            {
+                return ((Function)priority).function switch
+                {
+                    'q' => Math.Sqrt(number1.number),
+                    's' => Math.Sin(number1.number),
+                    'c' => Math.Cos(number1.number),
+                    't' => Math.Tan(number1.number),
+                    'g' => 1.0 / Math.Tan(number1.number),
+                    'a' => Math.Asin(number1.number),
+                    'b' => Math.Abs(number1.number),
+                    'n' => Math.Log(number1.number),
+                    'i' => Math.Log10(number1.number),
+                    _ => 0.0,
+                };
+            }
         }
 
-        public static double Calculating(List<Token> rpn)
+        public static double Calculating(List<Token> rpn, double x)
         {
             Stack<double> boofer = new();
             foreach (Token tokens in rpn)
             {
-
+                Number firstNum = new();
+                Number secondNum = new();
+                firstNum.number = 10.0;
                 if (tokens is Operation)
                 {
-                    Number firstNum = new();
-                    Number secondNum = new();
                     double number2 = boofer.Pop();
-                    double number1 = boofer.Pop();
-                    firstNum.number = number1;
+                    if (boofer.Count >= 1)
+                    {
+                        double number1 = boofer.Pop();
+                        firstNum.number = number1;
+                    }
                     secondNum.number = number2;
-                    double priority = GetNumber((Number)firstNum, (Number)secondNum, (Operation)tokens);
+                    double priority = GetNumber((Number)firstNum, (Number)secondNum, tokens);
                     boofer.Push(priority);
                 }
-                else if (tokens is Number numberpup)
-                    boofer.Push(numberpup.number);
+                else if (tokens is Number)
+                {
+                    boofer.Push(((Number)tokens).isVariable ? x : ((Number)tokens).number);
+                }
+                else if (tokens is Function)
+                {
+                    double number1 = boofer.Pop();
+                    firstNum.number = number1;
+                    secondNum.number = 1.0;
+                    double priority = GetNumber((Number)firstNum, (Number)secondNum, tokens);
+                    boofer.Push(priority);
+                }
             }
             return boofer.Peek();
         }
 
-
-        static void Print(List<Token> ListToPrint, Number number, Operation op, Parenthesis bracket)
+        public static readonly Dictionary<char, string> FunctionDict = new ()
         {
-            foreach (Token bringOut in ListToPrint)
+            { 'l', "log" },
+            { 'q', "sqrt" },
+            { 'r', "rt" },
+            { 's', "sin" },
+            { 'c', "cos" },
+            { 't', "tg" },
+            { 'g', "ctg" },
+            { 'a', "asin" },
+            { 'b', "abs" },
+            { 'n', "ln" },
+            { 'i', "lg" },
+            { '+', "+" },
+            { '-', "-" },
+            { '/', "/" },
+            { '*', "*" },
+            { '^', "^" }
+
+        };
+        static void Print(List<Token> tokens)
+        {
+            foreach (Token token in tokens)
             {
-                if (bringOut is Number)
+                if (token is Number num)
                 {
-                    number = (Number)bringOut;
-                    Console.Write(number.number);
-                    Console.Write(" ");
+                    Console.Write(num.isVariable ? "x " : $"{num.number} ");
                 }
-                else if (bringOut is Operation)
+                else if (token is Operation op)
                 {
-                    op = (Operation)bringOut;
-                    Console.Write(op.operation);
-                    Console.Write(" ");
+                    Console.Write($"{FunctionDict[op.operation]} ");
                 }
-                else
+                else if (token is Function func)
                 {
-                    bracket = (Parenthesis)bringOut;
-                    if (bracket.bracket) Console.Write("( ");
-                    else Console.Write(") ");
+                    Console.Write($"{FunctionDict[func.function]} ");
+                }
+                else if (token is Parenthesis par)
+                {
+                    Console.Write(par.bracket ? "( " : ") ");
                 }
             }
             Console.Write("\n");
